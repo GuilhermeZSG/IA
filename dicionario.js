317cdf0fc0b2e51713e891afe3cf1d367271867b
Guilherme Guerrero <guilherme.zsguerrero@senacsp.edu.br>
2026-05-15 22:23:41 -0300

    Mudanças na aula do 15/05/26

--- dicionario.js
+++ dicionario.js
@@ -1,14 +1,32 @@
 // instalar depedencia ou biblioteca
 // npm install @google/generative-ai
 
+// Importar a biblioteca
+const { GoogleGenerativeAI } = require("@google/generative-ai")
+
+// Inicializando o modelo de inteligencia artificial, usando a chave API como autenticação.
+const googleIA = new GoogleGenerativeAI("AIzaSyC99NuCRrD6yybL15u_ye_OAzQA617sfKE")  // Insira a chave da API
+const model = googleIA.getGenerativeModel({
+    model: "gemini-2.5-flash"
+})
 
 const palavras = ["Resiliencia", "Algoritmo", "Efemero", "Escopo", "Paralelograma"]
 
 async function criarDicionario() {
+    console.log("--- Dicionario Gerado por IA ---\n")
 
+    for (const palavra of palavras){
+        const prompt = `Atue como um dicionario. Forneça uma definição para a palavra: ${palavra}`
 
+        const resultado = await model.generateContent(prompt)
+        const resposta = await resultado.response
+        const definicao = resposta.text()
 
-    
+        console.log(`Palavra ${palavra}`)
+        console.log(`Definição ${definicao}`)
+        console.log(`-----------------------`)
+    }
+
 }
 
 criarDicionario()
---
+++ readme.md
@@ -0,0 +1,3 @@
+*** INDICE DA PASTA ***
+
+`dicionario.js`: Criando um dicionario a partir da API da ia do google.
